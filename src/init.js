cfcec12cc9a560b70b466a3e367eee1a27a1ea3b
Stephen Choe <stphnchoe@gmail.com>
2017-12-05 00:01:52 -0800

    Change Class functions to Pseudoclassical - Need to Fix BlinkyDancer position

--- src/init.js
+++ src/init.js
@@ -18,16 +18,17 @@ $(document).ready(function() {
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window['MakeBlinkyDancer'];
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    windows.dancer.push(dancer);
   });
 });
 
